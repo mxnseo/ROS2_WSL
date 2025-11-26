628022ee6db0fbf3d737988d8d46124be2b53f92
mxnseo <5l8ct@naver.com>
2025-11-26 15:11:19 +0900

    Fix nested git problem

---
+++ src/dxl_wsl/src/pub.cpp
@@ -0,0 +1,99 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+#include <memory>
+//#include <chrono>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#define STDIN_FILENO 0
+
+int getch(void) // 엔터키 입력 없이 한 문자만 입력시 바로 리턴
+{
+    struct termios oldt, newt;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return ch;
+}
+
+bool kbhit(void) // 키보드가 눌렸는지 체크해주는 함수
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return true;
+    }
+    return false;
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("node_dxlpub");
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+    auto mypub = node->create_publisher<geometry_msgs::msg::Vector3>("topic_dxlpub", qos_profile );
+    geometry_msgs::msg::Vector3 vel;
+
+    vel.x = 0; // 초기화
+    vel.y = 0;
+    vel.z = 0;
+    
+    rclcpp::WallRate loop_rate(20.0); //20hz->50msec
+
+    // 좌우 속도 명령 제어하기 위한 변수
+    int vel1=0,vel2=0;
+    int goal1=0,goal2=0;
+   
+    while(rclcpp::ok())
+    {
+        if (kbhit())
+        {
+            char c = getch(); // 키보드가 눌린 경우 한 문자만 입력, 엔터 필요없음
+            switch(c)
+            {
+            case 's': goal1 = 0; goal2 = 0; break;
+            case ' ': goal1 = 0; goal2 = 0; break;
+            case 'f': goal1 = 50; goal2 = -50; break;
+            case 'b': goal1 = -50; goal2 = 50; break;
+            case 'l': goal1 = -50; goal2 = -50; break;
+            case 'r': goal1 = 50; goal2 = 50; break;
+            default : goal1 = 0; goal2 = 0; break;
+            }         
+        }
+        
+        // generate accel and decel motion
+        if(goal1>vel1) vel1 += 5;
+        else if(goal1<vel1) vel1 -= 5;
+        else vel1 = goal1;
+
+        if(goal2>vel2) vel2 += 5;
+        else if(goal2<vel2) vel2 -= 5;
+        else vel2 = goal2;
+        vel.x = vel1;
+        vel.y = vel2;               
+        
+        RCLCPP_INFO(node->get_logger(), "Publish: %lf,%lf", vel.x, vel.y);
+        mypub->publish(vel);
+        loop_rate.sleep();
+    }
+    rclcpp::shutdown();
+    return 0;
+}
