f963d09ba03a4b891fd2d6f3517af20ad7a013c4
mxnseo <5l8ct@naver.com>
2025-12-03 15:42:14 +0900

    line detect & color

--- src/linedetect_wsl/src/sub.cpp
+++ src/linedetect_wsl/src/sub.cpp
@@ -14,17 +14,70 @@ void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::Compre
     cv::Mat frame_gray;
     cv::cvtColor(frame_color, frame_gray, cv::COLOR_BGR2GRAY);
 
+    cv::Scalar bright_avg = mean(frame_gray); // 밝기 평균
+    frame_gray = frame_gray + (100 - bright_avg[0]);
+
+    // 이진화 후 crop
     cv::Mat frame_binary;
-    cv::threshold(frame_gray, frame_binary, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+    cv::threshold(frame_gray, frame_binary, 150, 255, cv::THRESH_BINARY);
     
     cv::Mat roi = frame_binary(cv::Rect(0, 240, 640, 120));
 
+    
+    // 객체 찾는 부분
+    static cv::Point tmp_pt(320, 60);
+    cv::Mat labels, stats, centroids;
+    int cnt = cv::connectedComponentsWithStats(roi, labels, stats, centroids);
+
+    int min_idx = -1;
+    int min_dist = roi.cols;
+
+    for (int i=1; i<cnt; i++){
+        int area = stats.at<int>(i, 4);
+
+        if(area > 100){
+            int x = cvRound(centroids.at<double>(i, 0));
+            int y = cvRound(centroids.at<double>(i, 1));
+            int dist = cv::norm(cv::Point(x, y) - tmp_pt);
+
+            if(dist < min_dist && dist <= 150){
+                min_dist = dist;
+                min_idx = i;
+            }
+        }
+    }
+
+    if(min_idx != -1 && min_dist <= 150){
+        tmp_pt = cv::Point(cvRound(centroids.at<double>(min_idx, 0)), cvRound(centroids.at<double>(min_idx, 1)));
+    }
+    else cv::circle(roi, cv::Point(tmp_pt.x, tmp_pt.y), 5, cv::Scalar(0, 0, 255), -1);
+
+    cv::Mat result;
+    cv::cvtColor(roi, result, cv::COLOR_GRAY2BGR);
+
+    for (int i = 1; i < stats.rows; i++) {
+       int area = stats.at<int>(i, 4);
+       if (area > 100) {
+           int x = cvRound(centroids.at<double>(i, 0));
+           int y = cvRound(centroids.at<double>(i, 1));
+           if (x == tmp_pt.x) {
+               cv::rectangle(result, cv::Rect(stats.at<int>(i, 0), stats.at<int>(i, 1), stats.at<int>(i, 2), stats.at<int>(i, 3)), cv::Scalar(0, 0, 255));
+               cv::circle(result, cv::Point(x, y), 5, cv::Scalar(0, 0, 255), -1);
+           }
+           else {
+               cv::rectangle(result, cv::Rect(stats.at<int>(i, 0), stats.at<int>(i, 1), stats.at<int>(i, 2), stats.at<int>(i, 3)), cv::Scalar(255, 0, 0));
+               cv::circle(result, cv::Point(x, y), 5, cv::Scalar(255, 0, 0), -1);
+           }
+           
+       }
+    }
     cv::imshow("frame_color", frame_color);
-    cv::imshow("frame_roi", roi);
+    cv::imshow("frame_roi", result);
 
     cv::waitKey(1);
     RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame_color.rows, frame_color.cols);
 }
+
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
